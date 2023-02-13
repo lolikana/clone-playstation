5eddb4e9480ad4e57dd6a39a7273be10039eeebb
Lolikana <thomas.guiart@gmail.com>
2023-02-13 16:21:03 +0900

    change thumbs module for controller

--- src/ts/swiper.ts
+++ src/ts/swiper.ts
@@ -3,19 +3,22 @@ import Swiper, {
   Controller,
   EffectFade,
   Keyboard,
-  SwiperOptions,
-  Thumbs
+  SwiperOptions
 } from 'swiper';
 import 'swiper/css/effect-fade';
 import 'swiper/css/thumbs';
 import 'swiper/css';
 
 const thumbsParams: SwiperOptions = {
-  modules: [Keyboard, Controller, Autoplay],
+  modules: [Keyboard, Autoplay, Controller],
   loop: true,
   slidesPerView: 'auto',
-  spaceBetween: '2%',
+  spaceBetween: 5,
   centeredSlides: true,
+  slideToClickedSlide: true,
+  grabCursor: true,
+  watchSlidesProgress: true,
+  keyboard: true,
   breakpoints: {
     700: {
       slidesPerView: 7,
@@ -23,37 +26,34 @@ const thumbsParams: SwiperOptions = {
       spaceBetween: 7
     }
   },
-  slideToClickedSlide: true,
-  grabCursor: true,
-  watchSlidesProgress: true,
-  keyboard: true,
   autoplay: {
     delay: 3000,
     stopOnLastSlide: true,
     disableOnInteraction: true,
     pauseOnMouseEnter: true,
     waitForTransition: true
-  }
+  },
+  speed: 500
 };
 
 const galleryTopThumbs = new Swiper('.section--top-thumbnails', thumbsParams);
 
 const sliderParams: SwiperOptions = {
-  modules: [Thumbs, EffectFade, Controller, Autoplay],
+  modules: [EffectFade, Controller],
   effect: 'fade',
   fadeEffect: {
     crossFade: true
   },
   loop: true,
-  thumbs: {
-    swiper: galleryTopThumbs
+  loopedSlides: 6,
+  controller: {
+    control: galleryTopThumbs
   },
   speed: 500
 };
 
-new Swiper('.section--top-swiper', sliderParams);
-
-// const slider = new Swiper('.mySwiper2', sliderParams);
+const slider = new Swiper('.section--top-swiper', sliderParams);
+galleryTopThumbs.controller.control = slider;
 
 // galleryThumbs.controller.control = slider;
 // slider.controller.control = galleryThumbs;
