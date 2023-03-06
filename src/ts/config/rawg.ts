f09de7996c71666644f2b58a1b9e410fe73eea3c
Lolikana <thomas.guiart@gmail.com>
2023-03-06 16:43:23 +0900

    comment fetch api

--- src/ts/config/rawg.ts
+++ src/ts/config/rawg.ts
@@ -1,12 +1,11 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-import { normalizeDate } from '../utils/helper';
+// import { normalizeDate } from '../utils/helper';
 
-const today = normalizeDate(new Date());
-const nextThirtyDays = normalizeDate(new Date(Date.now() + 1000 * 60 * 60 * 24 * 30));
-const lastThirtyDays = normalizeDate(new Date(Date.now() - 1000 * 60 * 60 * 24 * 30));
+// const today = normalizeDate(new Date());
+// const nextThirtyDays = normalizeDate(new Date(Date.now() + 1000 * 60 * 60 * 24 * 30));
+// const lastThirtyDays = normalizeDate(new Date(Date.now() - 1000 * 60 * 60 * 24 * 30));
 
-console.log(nextThirtyDays, lastThirtyDays);
 const listGamesNewReleasesWrapper = document.querySelector(
   '.section--listGames-newReleases .section--listGames-wrapper'
 ) as HTMLDivElement;
@@ -30,37 +29,49 @@ const createListSlide = (data: { img: string; title: string }) => {
   return div;
 };
 
-axios
-  .get(
-    `https://api.rawg.io/api/games?platforms=187&page_size=12&dates=${lastThirtyDays},${today}&ordering=released&key=${process.env.RAWG_KEY}`
-    // ''
-  )
-  .then((res: any) => {
-    res.data.results.map((game: any) => {
-      console.log(game);
-      if (game) {
-        listGamesNewReleasesWrapper.appendChild(
-          createListSlide({ title: game.name, img: game.background_image })
-        );
-      }
-      return game;
-    });
-  })
-  .catch((err: unknown) => console.log(err));
-axios
-  .get(
-    `https://api.rawg.io/api/games?platforms=187&page_size=12&dates=${today},${nextThirtyDays}&ordering=released&key=${process.env.RAWG_KEY}`
-    // ''
-  )
-  .then((res: any) => {
-    res.data.results.map((game: any) => {
-      console.log(game);
-      if (game) {
-        listGamesComingSoonWrapper.appendChild(
-          createListSlide({ title: game.name, img: game.background_image })
-        );
-      }
-      return game;
-    });
-  })
-  .catch((err: unknown) => console.log(err));
+for (let i = 1; i <= 12; i++) {
+  listGamesNewReleasesWrapper.appendChild(
+    createListSlide({
+      title: 'The Last of Us',
+      img: 'https://upload.wikimedia.org/wikipedia/en/4/46/Video_Game_Cover_-_The_Last_of_Us.jpg'
+    })
+  );
+  listGamesComingSoonWrapper.appendChild(
+    createListSlide({
+      title: 'The Last of Us',
+      img: 'https://upload.wikimedia.org/wikipedia/en/4/46/Video_Game_Cover_-_The_Last_of_Us.jpg'
+    })
+  );
+}
+
+// axios
+//   .get(
+//     `https://api.rawg.io/api/games?platforms=187&page_size=12&dates=${lastThirtyDays},${today}&ordering=released&key=${process.env.RAWG_KEY}`
+//   )
+//   .then((res: any) => {
+//     res.data.results.map((game: any) => {
+//       if (game) {
+//         listGamesNewReleasesWrapper.appendChild(
+//           createListSlide({ title: game.name, img: game.background_image })
+//         );
+//       }
+//       return game;
+//     });
+//   })
+//   .catch((err: unknown) => console.log(err));
+
+// axios
+//   .get(
+//     `https://api.rawg.io/api/games?platforms=187&page_size=12&dates=${today},${nextThirtyDays}&ordering=released&key=${process.env.RAWG_KEY}`
+//   )
+//   .then((res: any) => {
+//     res.data.results.map((game: any) => {
+//       if (game) {
+//         listGamesComingSoonWrapper.appendChild(
+//           createListSlide({ title: game.name, img: game.background_image })
+//         );
+//       }
+//       return game;
+//     });
+//   })
+//   .catch((err: unknown) => console.log(err));
